97417f244590042157778354e8b0d1fba93cb31f
Rohit Jain <jrohit081@gmail.com>
2022-03-21 09:18:17 +0530

    First Bit Changes Commit

--- JS/FindFirstSetBit.js
+++ JS/FindFirstSetBit.js
@@ -1,11 +1,13 @@
 const n1 = 18;
 const n2 = 20;
 const n3 = 3365;
+const n4 = 15;
+const n5 = 0;
 
 function getFirstSetBit(n) {
   let position = 1;
   if (n == 0) {
-    return 0;
+    return console.log(0);
   }
   while (n > 0) {
     if ((n & 1) == 1) {
@@ -20,3 +22,5 @@ function getFirstSetBit(n) {
 getFirstSetBit(n1);
 getFirstSetBit(n2);
 getFirstSetBit(n3);
+getFirstSetBit(n4);
+getFirstSetBit(n5);
